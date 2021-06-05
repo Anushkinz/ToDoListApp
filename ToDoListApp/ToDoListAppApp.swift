16af6f377472e2921bd79fd08967d38129b07cdc
Anushkinz <anuarprogramm@gmail.com>
2021-06-05 12:58:32 +0600

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by admin on 5/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
